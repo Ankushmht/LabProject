ed9be98b1a11b2d2b928e22e848f8787496dcd19
ankushmht <ankushm@damcogroup.com>
2019-10-31 19:47:57 +0530

    Added Edit for test

--- LabProject/Controllers/TestController.cs
+++ LabProject/Controllers/TestController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -60,6 +60,18 @@ namespace LabProject.Controllers
                                     PreRequisite = t.PreRequisite
                                 }).ToList().FirstOrDefault();
             return View(tests);
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(Test tst)
+        {
+            Test test = labEnity.Tests.FirstOrDefault(x => x.Testid == tst.Testid);
+            test.TestName = tst.TestName;
+            test.TestReportDuration = tst.TestReportDuration;
+            test.PreRequisite = tst.PreRequisite;
+            labEnity.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 
