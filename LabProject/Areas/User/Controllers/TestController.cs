00f34b46ef7b099b162d8ae6db11a75dfd05aa0e
ankushmht <ankushm@damcogroup.com>
2019-11-02 14:18:42 +0530

    userchanges

---
+++ LabProject/Areas/User/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabProject.Areas.User.Controllers
+{
+    public class TestController : Controller
+    {
+        LabDataBaseEntities labEnity = new LabDataBaseEntities();
+        // GET: Test
+        public ActionResult Index()
+        {
+            List<Test> tests = labEnity.Tests.ToList();
+            return View(tests);
+        }
+    }
+}
