b7a1f766b5aa2624fc1e50b1045de3be4b52f6a6
ankushmht <ankushm@damcogroup.com>
2019-11-15 17:45:40 +0530

    commit

--- LabProject/Areas/User/Controllers/CartController.cs
+++ LabProject/Areas/User/Controllers/CartController.cs
@@ -16,16 +16,39 @@ namespace LabProject.Areas.User.Controllers
         [HttpGet]
         public ActionResult GetCart()
         {
-            List<BookTest> lstBooktest =(List<BookTest>)Session["lstBookTest"];
-            lstBooktest=lstBooktest.Where(x => x.UserId == Convert.ToInt32(Request.Cookies["userId"].Value)).ToList();
-            List<TestLabPrice> lsttlp = new List<TestLabPrice>();
+            if (Request.Cookies["userId"].Value != "")
+            {
+                List<BookTest> lstBooktest = (List<BookTest>)Session["lstBookTest"];
 
-            foreach (var x in lstBooktest) {
-                TestLabPrice tlp = new TestLabPrice();
-                tlp=labEntity.TestLabPrices.Where(xx => xx.TestLabId == x.TestLabId).FirstOrDefault();
-                lsttlp.Add(tlp);
+
+                lstBooktest = lstBooktest.Where(x => x.UserId == Convert.ToInt32(Request.Cookies["userId"].Value)).ToList();
+                List<TestLabPrice> lsttlp = new List<TestLabPrice>();
+
+                foreach (var x in lstBooktest) {
+                    TestLabPrice tlp = new TestLabPrice();
+                    tlp = labEntity.TestLabPrices.Where(xx => xx.TestLabId == x.TestLabId).FirstOrDefault();
+                    lsttlp.Add(tlp);
+                }
+
+                List<TestLab> testlab;
+                testlab = (from labt in lsttlp
+                           join lab in labEntity.LabDetails on labt.LabId equals lab.LabId
+                           join t in labEntity.Tests on labt.TestId equals t.Testid
+                           select new TestLab
+                           {
+                               testLabId = labt.TestLabId,
+                               tests = t,
+                               Labs = lab,
+                               Price = labt.Price
+                           }).ToList();
+                ViewBag.Total = testlab.Sum(data => data.Price);
+                return View(testlab);
             }
-            return View(lsttlp);
+            else
+            {
+                return View();
+            }
+
         }
     }
 }
